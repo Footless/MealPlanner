5f65b5ff682ca6ceecb3c06c421afaed0134be64
Kari Vänttinen <kvanttinen@gmail.com>
2016-12-29 05:18:35 +0200

    meal nätimmäksi, testejä, uusimmat rapsat

--- MealPlanner/src/main/java/kari/nutritionplanner/mealplanner/domain/Meal.java
+++ MealPlanner/src/main/java/kari/nutritionplanner/mealplanner/domain/Meal.java
@@ -5,173 +5,138 @@
  */
 package kari.nutritionplanner.mealplanner.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author kari
  */
 public class Meal {
 
-    private Ingredient mainIngredient;
-    private Ingredient sideIngredient;
-    private Ingredient sauce;
-    private Ingredient misc;
-    private double mainIngredientAmount;
-    private double sideIngredientAmount;
-    private double sauceAmount;
-    private double miscAmount;
+    private Map<Integer, Ingredient> ingredients;
+    private Map<Integer, Double> amounts;
+    
+    public Meal() {
+        this.ingredients = new HashMap<>();
+        this.amounts = new HashMap<>();
+        this.amounts.put(1, 0.0);
+        this.amounts.put(2, 0.0);
+        this.amounts.put(3, 0.0);
+        this.amounts.put(4, 0.0);
+        this.ingredients.put(1, new Ingredient(0, "dump"));
+        this.ingredients.put(2, new Ingredient(0, "dump"));
+        this.ingredients.put(3, new Ingredient(0, "dump"));
+        this.ingredients.put(4, new Ingredient(0, "dump"));
+    }
 
     public void setMainIngredient(Ingredient mainIngredient) {
-        this.mainIngredient = mainIngredient;
+        ingredients.replace(1, mainIngredient);
     }
 
     public void setSideIngredient(Ingredient sideIngredient) {
-        this.sideIngredient = sideIngredient;
+        ingredients.replace(4, sideIngredient);
     }
 
     public void setSauce(Ingredient sauce) {
-        this.sauce = sauce;
+        ingredients.replace(2, sauce);
     }
 
     public void setMisc(Ingredient misc) {
-        this.misc = misc;
+        ingredients.replace(3, misc);
     }
 
     public void setMainIngredientAmount(double mainIngredientAmount) {
-        this.mainIngredientAmount = mainIngredientAmount;
+        this.amounts.replace(1, mainIngredientAmount);
     }
 
     public void setSideIngredientAmount(double sideIngredientAmount) {
-        this.sideIngredientAmount = sideIngredientAmount;
+        this.amounts.replace(4, sideIngredientAmount);
     }
 
     public void setSauceAmount(double sauceAmount) {
-        this.sauceAmount = sauceAmount;
+        this.amounts.replace(2, sauceAmount);
     }
 
     public void setMiscAmount(double miscAmount) {
-        this.miscAmount = miscAmount;
+        this.amounts.replace(3, miscAmount);
     }
 
     public double getProtein() {
         double protein = 0;
-        if (mainIngredient != null) {
-            protein += mainIngredientAmount * mainIngredient.getProtein();
-        }
-        if (sauce != null) {
-            protein += sauceAmount * sauce.getProtein();
-        }
-        if (sideIngredient != null) {
-            protein += sideIngredientAmount * sideIngredient.getProtein();
-        }
-        if (misc != null) {
-            protein += miscAmount * misc.getProtein();
+        for (Integer i : ingredients.keySet()) {
+            protein += ingredients.get(i).getProtein() * amounts.get(i);
         }
         return protein;
     }
 
     public double getFat() {
         double fat = 0;
-        if (mainIngredient != null) {
-            fat += mainIngredientAmount * mainIngredient.getFat();
-        }
-        if (sauce != null) {
-            fat += sauceAmount * sauce.getFat();
-        }
-        if (sideIngredient != null) {
-            fat += sideIngredientAmount * sideIngredient.getFat();
-        }
-        if (misc != null) {
-            fat += miscAmount * misc.getFat();
+        for (Integer i : ingredients.keySet()) {
+            fat += ingredients.get(i).getFat() * amounts.get(i);
         }
         return fat;
     }
 
     public double getCalories() {
         double calories = 0;
-        if (mainIngredient != null) {
-            calories += mainIngredientAmount * mainIngredient.getCalories();
-        }
-        if (sauce != null) {
-            calories += sauceAmount * sauce.getCalories();
-        }
-        if (sideIngredient != null) {
-            calories += sideIngredientAmount * sideIngredient.getCalories();
-        }
-        if (misc != null) {
-            calories += miscAmount * misc.getCalories();
+        for (Integer i : ingredients.keySet()) {
+            calories += ingredients.get(i).getCalories() * amounts.get(i);
         }
         return calories;
     }
 
     public double getCarbs() {
         double carbs = 0;
-        if (mainIngredient != null) {
-            carbs += mainIngredientAmount * mainIngredient.getCarb();
-        }
-        if (sauce != null) {
-            carbs += sauceAmount * sauce.getCarb();
-        }
-        if (sideIngredient != null) {
-            carbs += sideIngredientAmount * sideIngredient.getCarb();
-        }
-        if (misc != null) {
-            carbs += miscAmount * misc.getCarb();
+        for (Integer i : ingredients.keySet()) {
+            carbs += ingredients.get(i).getCarb() * amounts.get(i);
         }
         return carbs;
     }
 
     public double getFiber() {
         double fibers = 0;
-        if (mainIngredient != null) {
-            fibers += mainIngredientAmount * mainIngredient.getFiber();
-        }
-        if (sauce != null) {
-            fibers += sauceAmount * sauce.getFiber();
-        }
-        if (sideIngredient != null) {
-            fibers += sideIngredientAmount * sideIngredient.getFiber();
-        }
-        if (misc != null) {
-            fibers += miscAmount * misc.getFiber();
+        for (Integer i : ingredients.keySet()) {
+            fibers += ingredients.get(i).getFiber() * amounts.get(i);
         }
         return fibers;
     }
 
     public Ingredient getSideIngredient() {
-        return sideIngredient;
+        return ingredients.get(4);
     }
 
     public double getMainIngredientAmount() {   //kerrotaan sadalla tulostusvaiheessa
-        return mainIngredientAmount;
+        return amounts.get(1);
     }
 
     public double getSauceAmount() {   //kerrotaan sadalla tulostusvaiheessa
-        return sauceAmount;
+        return amounts.get(2);
     }
 
     public Ingredient getSauce() {
-        return sauce;
+        return ingredients.get(2);
     }
 
     public Ingredient getMainIngredient() {
-        return mainIngredient;
+        return ingredients.get(1);
     }
 
     public double getMiscAmount() {   //kerrotaan sadalla tulostusvaiheessa
-        return miscAmount;
+        return amounts.get(3);
     }
 
     public double getSideIngredientAmount() {   //kerrotaan sadalla tulostusvaiheessa
-        return sideIngredientAmount;
+        return amounts.get(4);
     }
 
     @Override
     public String toString() {
         return "Ainekset:\n"
-                + mainIngredient + ": " + getMainIngredientAmount() * 100 + "gr\n"
-                + sideIngredient + ": " + getSideIngredientAmount() * 100 + "gr\n"
-                + sauce + ": " + getSauceAmount() * 100 + "gr\n"
-                + misc + ": " + getMiscAmount() * 100 + "gr\n\n"
+                + ingredients.get(1).getName() + ": " + getMainIngredientAmount() * 100 + "gr\n"
+                + ingredients.get(4).getName() + ": " + getSideIngredientAmount() * 100 + "gr\n"
+                + ingredients.get(2).getName() + ": " + getSauceAmount() * 100 + "gr\n"
+                + ingredients.get(3).getName() + ": " + getMiscAmount() * 100 + "gr\n\n"
                 + "Ravintoarvot: \n"
                 + "Kilokalorit: " + getCalories() + "kcal\n"
                 + "Proteiini: " + getProtein() + "gr\n"
